8376fa4b95f7c2cbd9c0cf4c11b27b25ac695000
Alfacoy <ninanbudevelop@gmail.com>
2017-06-04 23:11:17 -0300

    Corrector
    Se corrije un error de tipeo.

--- IaPorcentaje.js
+++ IaPorcentaje.js
@@ -1,5 +1,6 @@
 const hp = 5980
 let hpv = 3490
+//Esta funcion toma por atributos el HPV y HP multiplicando por cien al hpv y dividiendolo por hp para asi, tener un porcentaje redondeado de su vida y preguntarse que hacer.
 
 function answer(hpv,hp){
   const cien = 100
