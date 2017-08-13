247d5f2fb716be93f1e1044b418db174e63bb6ca
Alfacoy <ninanbudevelop@gmail.com>
2017-08-12 23:07:56 -0300

    Variables
    Todas las variables se encuentran aca.

---
+++ js/variables.js
@@ -0,0 +1,20 @@
+//VARIABLES GENERALES DEL JUEGO.
+var agresivo = false;
+var defensivo = false;
+var espada = false;
+var arco = false;
+
+//Datos del Jugador.
+var Clase = document.querySelector('#clase').innerHTML += " Desconocida!"
+var Nombre = document.querySelector('#nombre');
+var Salud = document.querySelector('#salud');
+var Defensa = document.querySelector('#defensa');
+var Ataque = document.querySelector('#fuerza');
+var Consola = document.querySelector('#consola').innerHTML = "Texto de Batalla.";
+//FIN Datos del Jugador.
+
+
+
+
+
+//FIN VARIABLES GENERALES DEL JUEGO.
